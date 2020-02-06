f0bd5865a726d3ab3753d59a8845cf0158218280
Maarja Tapsi <maarja.tapsi@khk.ee>
2020-02-06 09:30:54 +0200

    Login and logout

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 // config file
 require_once 'config/constants.php';
+// start session
+session_start();
+
 // Auto Load libraries
 spl_autoload_register(function ($className){
     require_once 'libraries/'.$className.'.php';
--- app/models/User.php
+++ app/models/User.php
@@ -39,7 +39,7 @@ class User
         $user = $this->db->getOne();
         $hashedPassword = $user->password;
         if(password_verify($password, $hashedPassword)){
-
+            return $user;
         } else {
             return false;
         }
